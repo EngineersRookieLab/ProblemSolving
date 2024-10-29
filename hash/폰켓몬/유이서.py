6237083c883fd02979d00c0e84888ea4029568c6
yu iseo <97578465+yuiseo@users.noreply.github.com>
2024-10-29 22:51:26 +0900

    Create 유이서.py

---
+++ hash/폰켓몬/유이서.py
@@ -0,0 +1,16 @@
+def solution(nums):
+    answer = 0
+    dictionary = {}
+    
+    for n in nums:
+        if not n in dictionary:
+            dictionary[n] = 1
+        else:
+            dictionary[n] +=1
+    
+    if len(dictionary) >= len(nums)//2:
+        answer = len(nums)//2
+    else:
+        answer = len(dictionary)
+    
+    return answer
