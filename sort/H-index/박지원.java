42014eced7bd26e138810b8051b12d120ae5764f
Jiwon Park <37170306+since1909@users.noreply.github.com>
2024-11-14 15:50:51 +0900

    Add #4 H-index - 박지원

---
+++ sort/H-index/박지원.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        for(int i = 0; i < citations.length; i++) {
+            int h = citations.length - i;
+            if(h <= citations[i]) 
+                return h;
+        }
+        return 0;
+    }
+}
