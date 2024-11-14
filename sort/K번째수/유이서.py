c5ed56d554be4e9266049f3511aad07fb196bb66
yu iseo <97578465+yuiseo@users.noreply.github.com>
2024-11-14 14:07:27 +0900

    Add: #4 [sort]K번째수 - 유이서

---
+++ sort/K번째수/유이서.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+    
+    for i,j,k in commands:
+        editArr = sorted(array[i-1:j])
+        answer.append(editArr[k-1])
+        
+    return answer
