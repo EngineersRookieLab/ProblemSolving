d7a45baa40c9bc5d0fb907a009ebc9b5b3a4779d
지선아 <jisuna314@gmail.com>
2025-01-16 21:40:45 +0900

    Update #3 주식가격 - 지선아
    큐 -> 스택으로 변경해서 다시 풀었음

--- stack&queue/주식가격/지선아.java
+++ stack&queue/주식가격/지선아.java
@@ -22,3 +22,28 @@ class Solution {
     }
 
 }
+
+// stack 활용
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+       int n = prices.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        // 스택에는 가격이 떨어지지 않은 인덱스만 저장해!!!
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int index = stack.pop();
+                answer[index] = i - index;
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            int index = stack.pop();
+            answer[index] = n - index - 1;
+        }
+        return answer;
+    }
+
+}
